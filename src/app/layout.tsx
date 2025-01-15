6199f8111ef3917bc1a6829fd41980d739ac5ab6
Ram Krishna Paudel <ramkrishnaimmortal@gmail.com>
2025-01-15 16:43:41 +0545

    favicon issue fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,11 +14,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-      <link rel="icon" href="./favicon.ico" />
-      <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
-        <link rel="manifest" href="./site.webmanifest" />
+      {/* <link rel="icon" href="./favicon.ico" /> */}
+        <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+        {/* <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" /> */}
+        {/* <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" /> */}
+        {/* <link rel="manifest" href="./site.webmanifest" /> */}
         <title>Portfolio Ram Krishna Paudel</title>
       </head>
       <body className={inter.className}>
